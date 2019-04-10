1340d586355fea37218eb71c2b51fa68ba8a096a
Greg V <greg@unrelenting.technology>
2019-04-10 22:31:34 +0300

    Add geckodriver support (response parser, config)

--- lib/hound/connection_server.ex
+++ lib/hound/connection_server.ex
@@ -10,6 +10,8 @@ defmodule Hound.ConnectionServer do
         {9515, nil, "chrome"}
       "phantomjs" ->
         {8910, nil, "phantomjs"}
+      "geckodriver" ->
+        {4444, nil, "firefox"}
       _ -> # assume selenium
         {4444, "wd/hub/", "firefox"}
     end
